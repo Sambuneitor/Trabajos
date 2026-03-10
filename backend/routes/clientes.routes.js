0f310001969310997524a517d5111d3a7271cf40
Sambuneitor <audyma.net@gmail.com>
2026-03-10 17:35:25 -0500

    FIN
    10/03 - 2

---
+++ backend/routes/clientes.routes.js
@@ -0,0 +1,69 @@
+/** rutas del cliente
+ * rutas publicas y para los clientes autenticados
+ */
+
+const express = require('express');
+const router = express.Router();
+
+//importar los middlewares
+const { verificarAuth } = require('../middleware/auth');
+const { esCliente } = require('../middleware/checkRole');
+
+
+//importar controladores
+const catalogoController = require('../controllers/catalogo.controller');
+const carritoController = require('../controllers/carrito.controller');
+const pedidoController = require('../controllers/pedido.controller');
+
+
+//rutas publicas catalogo
+//GET /api/catalogo/productos
+router.get('/catalogo/productos', catalogoController.getProductos);
+
+//GET /api/catalogo/productos/:id
+router.get('/catalogo/productos/:id', catalogoController.getProductoById);
+
+//GET /api/catalogo/categorias
+router.get('/catalogo/categorias', catalogoController.getCategorias);
+
+//GET /api/catalogo/categorias/:id/subcategorias
+router.get('/catalogo/categorias/:id/subcategorias', catalogoController.getSubcategoriasPorCategorias);
+
+//GET /api/catalogo/destacados
+router.get('/catalogo/destacados', catalogoController.getProductosDestacados);
+
+
+//rutas del carrito
+//GET /api/cliente/carrito
+router.get('/cliente/carrito', verificarAuth, carritoController.getCarrito);
+
+//POST /api/cliente/carrito
+router.post('/cliente/carrito', verificarAuth, carritoController.agregarAlCarrito);
+
+//PUT /api/cliente/carrito/:id
+router.put('/cliente/carrito/:id', verificarAuth, carritoController.actualizarItemCarrito);
+
+//DELETE /api/cliente/carrito/:id
+//eliminar un item del carrito
+router.delete('/cliente/carrito/:id', verificarAuth, carritoController.eliminarItemCarrito);
+
+//DELETE /api/cliente/carrito/:id
+//vaciar carrito
+router.delete('/cliente/carrito', verificarAuth, carritoController.vaciarCarrito);
+
+
+//rutas de pedidos -cliente
+//POST /api/cliente/pedidos
+router.post('/cliente/pedidos', pedidoController.crearPedido);
+
+//GET /api/cliente/pedidos
+router.get('/cliente/pedidos', pedidoController.getMisPedidos);
+
+//GET /api/cliente/pedidos/:id
+router.get('/cliente/pedidos/:id', pedidoController.getPedidoById);
+
+//PUT /api/cliente/pedidos/:id/cancelar
+router.put('/cliente/pedidos/:id/cancelar', pedidoController.cancelarPedido);
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,215 @@
+/**
+ * SERVIDOR PRINCIPAL DEL BACKEND
+ * este es el archivo principal del servidor del backend
+ * condigura express, middleware, rutas y conexion de base de datos
+ */
+
+//importaciones
+
+//importar express para crear el server
+const express = require('express');
+
+//importar cors para permitir solicitudes desde el frontend
+const cors = require('cors');
+
+//importar path para manejar rutas de archivos
+const path = require('path');
+
+//importar dorenv para manejar variables de entorno
+require('dotenv').config();
+
+//importar cofiguracion de la base de datos
+const dbConfig = require('./backend/config/database');
+
+//importar modelos y adsociaciones
+const { initAssociations } = require('./backend/models')
+
+//importar seeders
+const { runSeeders } = require('./seeders/adminSeeder');
+const { timeStamp } = require('console');
+
+//crear aplicaciones express
+const app = express();
+
+//obtener el puerto desde la variable de entorno
+const PORT = process.env.PORT || 5000;
+
+//middlewares globales
+
+//cors permite peticiones dese el frontend
+//configura que los dominios pueden hacer periciones al backend
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    credentials: true,
+    methods: []
+    allowHeaders:
+}))
+
+/**
+ * express.urlencoded() pasar el body de los formularios
+ */
+
+app.use(express.urlencoded({ extended: true}));
+
+/**
+ * servir archivos estaticos imagenes desde la carpeta raiz
+ */
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+//middleware para loggin de peticiones
+//muestra en consola cada peticion que llega al server
+
+if(process.env.NODE_ENV === 'debelopment') {
+    app.use((req, res, next) => {
+        console.log(`ok ${req.method} ${req.path}`);
+        next();
+    });
+}
+
+//rutas
+
+//rutas raiz verificar que el servidor esta corriendo
+
+app.get('/,', (req, res) => {
+    res.json({
+        success: true,
+        message: 'servidor E-commerce Backend corriendo correctamente',
+        version: '1.0.0',
+        timeStamp: new Date().toISOString()
+    });
+});
+
+//ruta de salud verifica que el servidor como esta
+app.get('/api/health', (req, res) => {
+    res.json({
+        success: true,
+        status: 'healthy',
+        database: 'conected',
+        timeStamp: new Date().toISOString()
+    });
+});
+
+//rutas api
+
+//rutas de autenticacion
+//incluye registro login, perfil
+
+const authRoutes = require('./backend/routes/auth.routes');
+app.use('/api/auth', authRoutes);
+
+//rutas del administrador
+//requieren autenticacion y rol de administrador
+
+const adminRoutes = require('./backend/routes/admin.routes');
+app.use('/api/admin', adminRoutes);
+
+//rutas del cliente
+
+const clienteRoutes = require('./backend/routes/clientes.routes');
+app.use('/api/admin', clienteRoutes);
+
+//manejo de rutas no encontradas (404)
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        success: false,
+        message: 'ruta no encontrada',
+        path: req.path,
+    });
+});
+
+//manejo de errores globales
+
+app.use((err, req, res, next) => {
+    console.error('error:', err.message);
+    //error de multer subida de archivos
+    if (err.name === 'multerError') {
+        return res.status(400).json({
+            success: false,
+            message: 'error al subir el archivo',
+            error: err.message
+        });
+    }
+
+    //otros errores
+    res.status(500).json({
+        success: false,
+        message: err.message || 'error interno del servidor',
+        ...(process.env.NODE_ENV === 'development' && { stack: err.stack })
+    });
+});
+
+//inicializar servidor  y base de datos
+
+/**
+ * funcion principal para iniciar el servidor
+ * prueba la conexion a MySQL
+ * sincroniza los modelos (crea las tablas)
+ * inicia el servidor express
+ */
+
+const startServer = async () => {
+    try {
+        //paso 1 probar conexion a MySQL
+        console.log(' conectado a MySQL...');
+        const dbConnected = await testConnection();
+
+        if (!dbConnected) {
+            console.error('no se pudo conectar a MySQL verificar XAMPP y el archivo .env');
+            process.exit(1); //salir si no hay conexion
+        }
+
+        //paso 2 sincronizar modelos (crear tablas)
+        console.log('sincronizando modelos con la base de datos...');
+
+        //inicializar asocioaciones entre los modelos
+        initAssociations();
+
+        //en desarrollo alter puede ser true para actualizar la estructura
+        //en produccion debe ser false para no perder los datos
+        const alterTables = process.env.NODE_ENV === 'development';
+        const dbSynced = await syncDataBase(false, alterTables);
+
+        if (!dbSynced) {
+            console.error('x error al sincronizar la base de datos');
+            process.exit(1);
+        }
+
+        //paso 3 ejecutar seeders datos iniciales 
+        await runSeeders();
+
+        //paso 4 iniciar servidor express
+        app.listen(PORT, () => {
+            console.log(`\n ___________________`);
+            console.log(`servidor corriendo en el puerto ${PORT}`);
+            console.log(`URL http://localhost:${PORT}`);
+            console.log(`base de datos ${process.env.NODE_ENV}`);
+            console.log('servidor listo para realizar peticiones');
+        });
+    } catch (error) {
+        console.error('x error fatal al iniciar el servidor:', error.message);
+        process.exit(1);
+    }
+};
+
+//manejo de cierre 
+//captura el ctrl+c para cerrar el servidor correctamente
+
+process.on('SIGINT', () => {
+    console.log('\n\n cerrando servidor...');
+    process.exit(0);
+});
+
+//capturar errores no manejador 
+
+process.on('unhandledRejection', (err) => {
+    console.error('x error no menejado', err);
+    process.exit(1);
+});
+
+//iniciar servidor
+startServer();
+
+//exportar app para testing
+module.exports = app;
